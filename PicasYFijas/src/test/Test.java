15fc7c240759b057329133a131bfbe16deab773c
Carlos Lozano <carlosmapache1@gmailcom>
2020-11-19 15:08:26 -0500

    Metodo 2

--- PicasYFijas/src/test/Test.java
+++ PicasYFijas/src/test/Test.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class Test {
 
-        public static void main(String[] args) {
+        public static void main(String[] args) {        // Arroz con pollo
             Scanner scanner = new Scanner(System.in);//pollo
-            int num2, num1 = 0, picas = 0, fijas = 0, vidas= 1;
+            int num2, num1 = 0, picas = 0, fijas = 0, vidas= 1;  //Agua
             System.out.println("Cual va a ser la longitud del juego ");
             int longitude  = scanner.nextInt();
             int[] Num = new int[longitude];
@@ -14,4 +14,13 @@ public class Test {
 
 
         }
+            // Este va ser el metodo 2
+    /**
+     * Este es
+     *
+     * */
+        public void CheckRepeatedDigits(int[]arrayOne , int[]arrayTwo){
+
+    }
+
 }
