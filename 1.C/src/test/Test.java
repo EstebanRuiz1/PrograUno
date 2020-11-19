c4854dfc119994a8cbcda2dfb6e21d6456907d41
EstebanRuiz1 <eduardeste15@gmail.com>
2020-11-18 22:52:15 -0500

    Se sube el código del tercer ejercicio

--- 1.C/src/test/Test.java
+++ 1.C/src/test/Test.java
@@ -1,7 +1,15 @@
 package test;
 
+import models.Models;
+
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Puto");
+    Scanner sc = new Scanner(System.in);
+        Models models = new Models();
+        System.out.println("Ingrese la Hora");
+        boolean result = models.validateDate(sc.nextInt(), sc.nextInt(), sc.nextInt());
+        System.out.println("Su fecha es: "+result);
     }
 }
