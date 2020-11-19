77211e2a044ff5861f231c534bca71dc56293fbf
EstebanRuiz1 <eduardeste15@gmail.com>
2020-11-19 08:46:01 -0500

    se añaden la mayoría de metodos para hacer las conversiones

--- 3 Exercises/src/models/Models.java
+++ 3 Exercises/src/models/Models.java
@@ -7,29 +7,53 @@ public class Models {
                 this.doCelsiusFarenheit(data);
                 break;
             case 2:
-                //this.doFarenheitCelsius(data);
+                this.doFarenheitCelsius(data);
                 break;
             case 3:
+                this.doKelvinFarenheit(data);
                 break;
             case 4:
-                break;        
+                this.doFarenheitKelvin(data);
+                break;
             case 5:
+                this.doCelsiusKelvin(data);
                 break;
             case 6:
+                this.doKelvinRankine(data);
                 break;
             case 7:
+                this.doReaumurKelvin(data);
                 break;
             default:
                 throw new IllegalStateException("Este valor no se encuentra dentro del rango " );
         }
     }
 
+    private void doReaumurKelvin(int data) {
+        double converted = (data-32);
+    }
+
+    private void doKelvinRankine(int data) {
+        double converted = (data-32);
+    }
+
+    private void doCelsiusKelvin(int data) {
+        double converted = (data+273.15);
+    }
+
+    private void doFarenheitKelvin(int data) {
+        double converted = (data-32)*(5/9)+273.15;
+    }
+
+    private void doKelvinFarenheit(int data) {
+        double converted = (data-273.15)*(9/5)+32;
+    }
     private void doFarenheitCelsius(int data) {
-        double converted = ((data*(9/5))+32);
+        double converted = (data-32)*5/9;
         System.out.println("F a C"+data+converted);
     }
     private void doCelsiusFarenheit(int data) {
-        double converted = data-32 (5/9);
+        double converted = (data*(9/5)+32);
         System.out.println("C a F"+data+converted);
     }
 }
