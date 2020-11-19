dfd33d42b2981575691785f27e67c440ec33f4da
EstebanRuiz1 <eduardeste15@gmail.com>
2020-11-19 15:06:00 -0500

    Se limpia el codigo

--- PicasYFijas/src/test/Test.java
+++ PicasYFijas/src/test/Test.java
@@ -7,8 +7,11 @@ public class Test {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);//pollo
             int num2, num1 = 0, picas = 0, fijas = 0, vidas= 1;
-            int[] Num = new int[4];
-            int[] Num1 = new int[4];
+            System.out.println("Cual va a ser la longitud del juego ");
+            int longitude  = scanner.nextInt();
+            int[] Num = new int[longitude];
+            int[] Num1 = new int[longitude];
+
 
         }
 }
