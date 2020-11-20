376f8a4dd92bd390dac2e074cfa1e3c51d27e27e
carlosmapache1 <73614255+carlosmapache1@users.noreply.github.com>
2020-11-20 10:25:18 -0500

    Update Test.java

--- PicasYFijas/src/Test/Test.java
+++ PicasYFijas/src/Test/Test.java
@@ -1,7 +1,14 @@
 package Test;
 
 import java.util.Scanner;
-
+/**
+ * @author Carlos Hernado Lozano
+ * @author Esteban Ruiz 
+ * @author Jose Misael Paez
+ * TODO: Agregen a los otros dos que me da pereza
+ * 
+ * 
+ */
 public class Test {
 
     public static void main(String[] args) {
