2d22790f70c62e243044af3f605b16331b99169c
EstebanRuiz1 <eduardeste15@gmail.com>
2020-11-20 11:32:33 -0500

    se va estructurando el codigo

--- PicasYFijas/src/Test/Test.java
+++ PicasYFijas/src/Test/Test.java
@@ -1,5 +1,7 @@
 package Test;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 /**
  * @author Carlos Hernado Lozano
@@ -15,25 +17,10 @@ public class Test {
         int longitude = scanner.nextInt();
         int[] arrayTwo = new int[longitude];
         int[] arrayOne = new int[longitude];
-
     public static void main(String[] args) {
 
-        //Verifica si son fijas
-        for (int i = 0; i < longitude; i++) {
-                if (arrayTwo[i] == arrayOne[i]) {
-                    fijas += 1;
-            }
-        }
-        System.out.println("Dijitos fijos " + fijas);
-        //Verificar picasfdshjklfasdkhljsdfajklfsdkjañlkñljfa
-        for (int i = 0; i < longitude; i++) {
-            for (int j = 0; j < longitude; j++) {
-                if (arrayTwo[i] == arrayOne[j]&&arrayTwo[i]!=arrayOne[i]) {
-                    picas += 1;
-                }
-            }
-        }
-        System.out.println("Picas " + picas);
+
+
 
         for (int i = 0; i < arrayOne.length; i++) {
             if (arrayOne[i] == arrayTwo[i]) fijas++;
@@ -50,6 +37,26 @@ public class Test {
             System.out.println("ingrese el numero " + i + " de los numeros adivinadores");
             arrayTwo[i] = scanner.nextInt();
         }
+    }
+    public void verifyFijas(){
+        //Verifica si son fijas
+        for (int i = 0; i < longitude; i++) {
+            if (arrayTwo[i] == arrayOne[i]) {
+                fijas += 1;
+            }
+        }
+        System.out.println("Dijitos fijos " + fijas);
+    }
+    public void verifyPicas(){
+        //Verificar picas
+        for (int i = 0; i < longitude; i++) {
+            for (int j = 0; j < longitude; j++) {
+                if (arrayTwo[i] == arrayOne[j]&&arrayTwo[i]!=arrayOne[i]) {
+                    picas += 1;
+                }
+            }
+        }
+        System.out.println("Picas " + picas);
     }
     public void baseNumbers(){
 
