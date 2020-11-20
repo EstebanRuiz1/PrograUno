b1d1c8343f8b2b33347a42fe5275b6dcef324b9d
EstebanRuiz1 <eduardeste15@gmail.com>
2020-11-19 22:13:25 -0500

    Se deja funcionando el metodo para calcular picas y se necesita revision del metodo para calcular fijas

--- PicasYFijas/src/test/Test.java
+++ PicasYFijas/src/test/Test.java
@@ -8,11 +8,35 @@ public class Test {
         System.out.println("Cual va a ser la longitud del juego ");
         Scanner scanner = new Scanner(System.in);
         int picas = 0, fijas = 0;
-        int longitude  = scanner.nextInt();
+        int longitude = scanner.nextInt();
         int[] arrayOne = new int[longitude];
         int[] arrayTwo = new int[longitude];
-        for (int i = 0; i < longitude; i++) arrayOne[i] = scanner.nextInt();
-        for (int i = 0; i < longitude; i++) arrayTwo[i] = scanner.nextInt();
+        for (int i = 0; i < longitude; i++) {
+            System.out.println("Ingrese el numero " + i + " de los numeros a adivinar");
+            arrayOne[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < longitude; i++) {
+            System.out.println("ingrese el numero " + i + " de los numeros adivinadores");
+            arrayTwo[i] = scanner.nextInt();
+        }
+        //Verifica si son fijas
+        for (int i = 0; i < longitude; i++) {
+            for (int j = 0; j < longitude; j++) {
+                if (arrayTwo[j] == arrayOne[i]) {
+                    fijas += 1;
+                }
+            }
+        }
+        System.out.println("Dijitos fijos" + fijas);
+        for (int i = 0; i < longitude; i++) {
+            for (int j = 0; j < longitude; j++) {
+                if (arrayTwo[i] == arrayOne[j]) {
+                    picas += 1;
+                }
+            }
+        }
+        //Verificar picas
+        System.out.println("Picas" + picas);
         for (int i = 0; i < longitude; i++) {
             if (arrayOne[i] == arrayTwo[i])
                 picas++;
