3bcb1aa96278d50d170ce714e9e77dd873be7aee
carlosmapache1 <73614255+carlosmapache1@users.noreply.github.com>
2020-11-20 12:51:52 -0500

    Update Models.java
    commentario amigable

--- PicasYFijas/src/models/Models.java
+++ PicasYFijas/src/models/Models.java
@@ -62,7 +62,7 @@ public class Models {
     }
 
 
-
+    //Este metodo no sirve pa nada
     public void thisMethod() {              //Que chuchas es este metodo?
         for (int i = 0; i < arrayOne.length; i++) {
             if (arrayOne[i] == arrayTwo[i]) fijas++;
